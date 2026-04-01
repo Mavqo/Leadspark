dcb4240e972fdb346a5594a4a07c2d1d76f93f41
Mavqo <117902041+Mavqo@users.noreply.github.com>
2026-04-01 08:12:23 +0200

    fix: aggiunta integrazione @astrojs/tailwind per compilazione CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [
     react(),
+    tailwind(),
   ],
   output: 'server',
   adapter: node({
@@ -13,10 +15,5 @@ export default defineConfig({
   server: {
     port: process.env.PORT || 3000,
     host: true
-  },
-  vite: {
-    css: {
-      postcss: './postcss.config.js',
-    },
   },
 });
